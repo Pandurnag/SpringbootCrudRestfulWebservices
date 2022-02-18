f14d196fad43e250676d78cb22726f0b3ae18e99
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-02-18 23:18:07 +0530

    Springboot Crud Restful Webservices

---
+++ src/main/java/net/javaguides/springboot/entity/User.java
@@ -0,0 +1,59 @@
+package net.javaguides.springboot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastNmae;
+	
+	@Column(name = "email")
+	private String email;
+	
+	
+	public User(String firstName, String lastNmae, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastNmae = lastNmae;
+		this.email = email;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastNmae() {
+		return lastNmae;
+	}
+	public void setLastNmae(String lastNmae) {
+		this.lastNmae = lastNmae;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
